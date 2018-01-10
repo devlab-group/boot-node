2b25d5bd722846b4cfe88268719bcf0602b9fc7a
Revinand <mvgrechannikov@gmail.com>
2018-01-10 13:03:14 +0300

    [src] accept supported networks from command line arguments

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
 	"time"
 )
 
-var networkIDs = map[string]int{
-	"MAINNET": 1,
-	"TESTNET": 2,
+type arrayFlags []string
+
+func (flags *arrayFlags) Set(value string) error {
+	*flags = append(*flags, value)
+	return nil
+}
+
+func (flags *arrayFlags) String() string {
+	var buf bytes.Buffer
+	for _, arg := range *flags {
+		buf.WriteString(arg)
+	}
+	return buf.String()
 }
 
 type Peer struct {
@@ -20,16 +32,16 @@ type Peer struct {
 
 var peersList map[string]map[string]Peer
 var isStarted = false
+var networks arrayFlags
 
 // One day
 const peerExpireTime = 24 * 60 * 60
 
 func main() {
-	// Init empty peers maps for all possible networks
 	peersList = make(map[string]map[string]Peer)
-	for netId, _ := range networkIDs {
-		peersList[netId] = make(map[string]Peer)
-	}
+
+	flag.Var(&networks, "net", "Networks supported by this boot node")
+	flag.Parse()
 
 	http.HandleFunc("/peers", handlePeers)
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -48,23 +60,27 @@ func addPeerHandler(w http.ResponseWriter, r *http.Request) {
 	var peerData map[string]string
 	decodeRequestBody(w, r, &peerData)
 
-	netId := peerData["networkID"]
+	peerNetwork := peerData["network"]
 	address := peerData["address"]
 	pubKey := peerData["publicKey"]
 
-	if _, ok := networkIDs[netId]; !ok {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Unknown network ID\n"))
+	if ok := isNetworkSupported(peerNetwork); !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported network"))
 		return
 	}
 
 	if len(address) == 0 || len(pubKey) == 0 {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(""))
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Missed peer info"))
 		return
 	}
 
-	peersList[netId][pubKey] = Peer{
+	if _, ok := peersList[peerNetwork]; !ok {
+		peersList[peerNetwork] = make(map[string]Peer)
+	}
+
+	peersList[peerNetwork][pubKey] = Peer{
 		Addr:      address,
 		PubKey:    pubKey,
 		Timestamp: time.Now().Unix(),
@@ -95,10 +111,10 @@ func getPeersHandler(w http.ResponseWriter, r *http.Request) {
 	var peerData map[string]string
 	decodeRequestBody(w, r, &peerData)
 
-	netId := peerData["networkID"]
-	if _, ok := networkIDs[netId]; !ok {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Unknown network ID\n"))
+	netId := peerData["network"]
+	if ok := isNetworkSupported(netId); !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported network"))
 		return
 	}
 
@@ -114,7 +130,6 @@ func getPeersHandler(w http.ResponseWriter, r *http.Request) {
 	peersJson, err := json.Marshal(result)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(""))
 		return
 	}
 	w.Write(peersJson)
@@ -125,8 +140,20 @@ func decodeRequestBody(w http.ResponseWriter, r *http.Request, output *map[strin
 	err := decoder.Decode(output)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(""))
 		return
 	}
 	defer r.Body.Close()
+}
+
+func isNetworkSupported(network string) bool {
+	if len(networks) == 0 {
+		return true
+	}
+
+	for _, net := range networks {
+		if network == net {
+			return true
+		}
+	}
+	return false
 }
